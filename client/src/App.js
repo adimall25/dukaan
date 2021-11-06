f0ec7955cb15e81b83b8ac843bf8cc5e8d167b70
Yaswanth Reddy <66727274+yaswanthreddyyyr@users.noreply.github.com>
2021-11-06 12:29:05 +0530

    Made products clickable and redirects to new page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import Landing from './components/Landing';
 import BuyerHomePage from './components/Home/BuyerHomePage';
 import SellerHomePage from './components/Home/SellerHomePage';
@@ -7,44 +7,27 @@ import SellerProfile from './components/Profiles/SellerProfile';
 import Profiles from './components/Profiles/Profiles';
 import './css/Styles.css';
 import AddProduct from './components/Products/AddProduct';
-import getCookie from './utils/cookies/getCookie';
-import setCookie from './utils/cookies/setCookie';
+import SingleProductPage from './components/SingleProductPage';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Products from './components/Buyer/Products';
 
-//redux
-import {Provider} from 'react-redux';
-import store from "./store.js";
-
 function App() {
-  // useEffect(() => {
-  //   if(getCookie('buyer-token'))
-  //   {
-  //     store.getState
-  //   }
-  //   else if(getCookie('seller-token'))
-  //   {
-
-  //   }
-  //   else;
-  // }, [])
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/buyer/home" component={BuyerHomePage} />
-            <Route exact path="/buyer/home/products" component={Products} />
-            <Route exact path="/buyer/profile" component={BuyerProfile} />
-            <Route exact path="/seller/profile" component={SellerProfile} />
-            <Route exact path="/seller/products/add" component={AddProduct} />
-            <Route exact path="/seller/home" component={SellerHomePage} />
-          </Switch>
-        </Fragment>
-      </Router>
-    </Provider>
-    
+    <Router>
+      <Fragment>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/buyer/home" component={BuyerHomePage} />
+          <Route exact path="/buyer/home/products" component={Products} />
+          <Route exact path="/buyer/profile" component={BuyerProfile} />
+          <Route exact path="/seller/profile" component={SellerProfile} />
+          <Route exact path="/seller/products/add" component={AddProduct} />
+          <Route exact path="/seller/home" component={SellerHomePage} />
+          <Route exact path="/buyer/spp/:product_id" component={SingleProductPage}/>
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
