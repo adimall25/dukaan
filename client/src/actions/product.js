359cc1b132a800de53cfcfe863811fa6ec0e02fd
Adarsh Mall <adarshmall1000@gmail.com>
2021-11-10 20:44:36 +0530

    Add product linked with backend and redux

--- client/src/actions/product.js
+++ client/src/actions/product.js
@@ -1,4 +1,4 @@
-import { GET_LOGGED_SELLER_PRODUCTS,LOGGED_SELLER_PRODUCTS_LOADED, LOGGED_SELLER_PRODUCTS_ERROR  } from "./types"
+import { GET_LOGGED_SELLER_PRODUCTS,LOGGED_SELLER_PRODUCTS_LOADED, LOGGED_SELLER_PRODUCTS_ERROR, ADD_PRODUCT, PRODUCT_ADDED, ADD_PRODUCT_ERROR  } from "./types"
 import axios from "axios"
 
 
@@ -28,6 +28,44 @@ export const getLoggedInSellerProducts = () => {
             dispatch({
                 type : LOGGED_SELLER_PRODUCTS_ERROR,
                 payload : err
+            })
+        }
+        
+    }
+}
+
+export function addProduct({productData, history})
+{
+    return async(dispatch) => {
+
+        try
+        {
+            dispatch({
+                type : ADD_PRODUCT
+            })
+    
+            const body = {
+                productData
+            }
+    
+            const res = await axios.post("http://localhost:5000/api/products/add", body, {
+                headers : {
+                    'x-auth-token' : localStorage.getItem('seller-token')
+                }
+            })
+    
+            dispatch({
+                type : PRODUCT_ADDED,
+                payload : res.data.product
+            })
+    
+            history.push('/seller/home')
+        }
+        catch(err)
+        {
+            dispatch({
+                type:ADD_PRODUCT_ERROR,
+                payload: err
             })
         }
         
--- client/src/reducers/product.js
+++ client/src/reducers/product.js
@@ -1,4 +1,4 @@
-import { GET_LOGGED_SELLER_PRODUCTS, LOGGED_SELLER_PRODUCTS_ERROR, LOGGED_SELLER_PRODUCTS_LOADED } from "../actions/types";
+import { GET_LOGGED_SELLER_PRODUCTS, LOGGED_SELLER_PRODUCTS_ERROR, LOGGED_SELLER_PRODUCTS_LOADED, ADD_PRODUCT, ADD_PRODUCT_ERROR, PRODUCT_ADDED } from "../actions/types";
 
 const initialState = {
     products : [],
@@ -12,13 +12,20 @@ export default function(state = initialState, action)
 
     switch(type)
     {
+        case ADD_PRODUCT:
         case GET_LOGGED_SELLER_PRODUCTS:
             return {...state, loading: true}
         
         case LOGGED_SELLER_PRODUCTS_LOADED:
             return {...state, products : payload, loading:false}
+        case PRODUCT_ADDED:
+            return {
+                ...state,
+                products : state.products.concat([payload])
+            }
         
         case LOGGED_SELLER_PRODUCTS_ERROR:
+        case ADD_PRODUCT_ERROR:
             return {...state, loading:false, error: payload}
 
         default:
